58f757bb26e4335cca8e21a224be9dcaeb04f212
Lihua <404467859@qq.com>
2018-05-23 21:01:18 +0800

    http后台请求

---
+++ src/main/java/com/mainiway/utils/SecretKeyUtil.java
@@ -0,0 +1,19 @@
+package com.mainiway.utils;
+
+import com.mainiway.common.util.code.MD5;
+
+/**
+ * @Author:张立华
+ * @Description:
+ * @Date：Created in 22:27 2018/5/22
+ * @Modified By:
+ */
+public class SecretKeyUtil {
+    public static String token(String userName , String password ){
+        return MD5.toMD5(userName + "|" + password );
+    };
+
+    public static String payPassword(String userName , String password ){
+        return MD5.toMD5(userName + "|" + password );
+    };
+}
