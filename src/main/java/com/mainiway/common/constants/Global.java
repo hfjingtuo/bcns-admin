6a5a158f03eba5677460c74bcc7e29637a7724b3
Lihua <404467859@qq.com>
2018-05-24 21:41:17 +0800

    支付模块

---
+++ src/main/java/com/mainiway/common/constants/Global.java
@@ -0,0 +1,13 @@
+package com.mainiway.common.constants;
+
+/**
+ * @Author:张立华
+ * @Description:
+ * @Date：Created in 21:37 2018/5/24
+ * @Modified By:
+ */
+public class Global {
+    public static final String PAY_MARGIN_REGISTER_URL = "http://localhost:8080/gpm-pay/zlpay/marginRegister";
+    public static final String PAY_MARGIN_SMS_URL = "http://localhost:8080/gpm-pay/zlpay/marginSms";
+
+}
